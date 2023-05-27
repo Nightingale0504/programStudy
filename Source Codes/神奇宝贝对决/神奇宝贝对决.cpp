68136a132364ca30257a37f279f98c5fdb54deff
Boxuan Zhang <zhangboxuan@xtcteam.onmicrosoft.com>
2023-05-27 11:21:46 +0800

    Update 神奇宝贝对决.cpp

--- Source Codes/神奇宝贝对决/神奇宝贝对决.cpp
+++ Source Codes/神奇宝贝对决/神奇宝贝对决.cpp
@@ -3,12 +3,38 @@
 #include<algorithm>
 using namespace std;
 #define int long long
+int n,t1,t2;
+int a,b;
+char ans;
 signed main(){
 	#ifndef ONLINE_JUDGE
 		freopen("nightingale.in","r",stdin);
 		freopen("nightingale.out","w",stdout);
 	#endif
-
+	scanf("%lld",&n);
+	while (n--){
+		scanf("%lld%lld",&t1,&t2);
+		if (t1==1 && t2==2){
+			a++;
+		}else if (t1==2 && t2==3){
+			a++;
+		}else if (t1==1 && t2==3){
+			b++;
+		}else if (t1==2 && t2==1){
+			b++;
+		}else if (t1==3 && t2==2){
+			b++;
+		}else if (t1==3 && t2==1){
+			a++;
+		}
+	}
+	if (a>b){
+		printf("a win");
+	}else if (b>a){
+		printf("b win");
+	}else{
+		printf("tie");
+	}
 	return 0;
 }
 

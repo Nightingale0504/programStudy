c8718cdbdaa79901e74e6ed7a83c6ceba8ba4c99
Boxuan Zhang <zhangboxuan@xtcteam.onmicrosoft.com>
2023-06-15 15:07:44 +0800

    Updated

--- Source Codes/杨辉三角/杨辉三角.cpp
+++ Source Codes/杨辉三角/杨辉三角.cpp
@@ -3,21 +3,24 @@
 #include<algorithm>
 using namespace std;
 #define int long long
-int n,a[25][25];
+#define maxN 1005
+int C[maxN][maxN];
+int n,m;
 signed main(){
 	#ifndef ONLINE_JUDGE
 		freopen("nightingale.in","r",stdin);
 		freopen("nightingale.out","w",stdout);
 	#endif
 	scanf("%lld",&n);
-	for (int i=1;i<=n;i++){
-		for (int j=1;j<=i;j++){
-			if (i==1 || i==j){
-				a[i][j]=1;
-			}else{
-				a[i][j]=a[i-1][j]+a[i-1][j-1];
-			}
-			printf("%lld ",a[i][j]);
+	for (int i=0;i<=n;i++){
+		C[i][0]=C[i][i]=1;
+		for (int j=1;j<i;j++){
+			C[i][j]=C[i-1][j]+C[i-1][j-1];
+		}
+	}
+	for (int i=0;i<=n;i++){
+		for (int j=0;j<=i;j++){
+			printf("%lld ",C[i][j]);
 		}
 		printf("\n");
 	}

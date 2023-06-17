811c777730ce56c27e91f0862961606e44065b13
Boxuan Zhang <zhangboxuan@xtcteam.onmicrosoft.com>
2023-06-17 11:15:41 +0800

    Updated

--- Source Codes/找零钱/找零钱.cpp
+++ Source Codes/找零钱/找零钱.cpp
@@ -1,16 +1,28 @@
 #include<iostream>
 #include<cstdio>
+#include<cstring>
 #include<algorithm>
 using namespace std;
 #define int long long
-int n,ans;
+int n,m,ans;
+int a[25],dp[105];
 signed main(){
-	#ifndef ONLINE_JUDGE
-		freopen("nightingale.in","r",stdin);
-		freopen("nightingale.out","w",stdout);
-	#endif
-	scanf("%lld",&n);
-	
+//	#ifndef ONLINE_JUDGE
+//		freopen("nightingale.in","r",stdin);
+//		freopen("nightingale.out","w",stdout);
+//	#endif
+	memset(dp,0x3f,sizeof(dp));
+	dp[0]=0;
+	scanf("%lld%lld",&n,&m);
+	for (int i=1;i<=m;i++){
+		scanf("%lld",&a[i]);
+	}
+	for (int i=1;i<=m;i++){
+		for (int j=a[i];j<=n;j++){
+			dp[j]=min(dp[j],dp[j-a[i]]+1);
+		}
+	}
+	ans=dp[n];
 	printf("%lld",ans);
 	return 0;
 }
--- Source Codes/采药/采药.cpp
+++ Source Codes/采药/采药.cpp
@@ -1,30 +1,30 @@
 #include<iostream>
 #include<cstdio>
+#include<algorithm>
 using namespace std;
-int w[105],val[105]; // w - ��� val - ��ֵ 
-int dp[105][1005]; // dp[i]=j - ���Ϊi�ı����洢������ֵΪj	 
-int main(){
+#define int long long
+int w[105],val[105];
+int dp[105][1005];
+signed main(){
 	#ifndef ONLINE_JUDGE
 		freopen("nightingale.in","r",stdin);
 		freopen("nightingale.out","w",stdout);
 	#endif
-	int t,m;// t - �������� m - ֲ����� 
-    scanf("%d%d",&t,&m);
-    for(int i=1;i<=m;i++){
-        scanf("%d%d",&w[i],&val[i]);
+	int t,m;
+    scanf("%lld%lld",&t,&m);
+    for (int i=1;i<=m;i++){
+        scanf("%lld%lld",&w[i],&val[i]);
     }
-    // 0-1���� 
-    for(int i=1;i<=m;i++){ // ����ֲ�� 
-        for(int j=t;j>=0;j--){ // ������������ - �Ӵ�->С 
-            if(j>=w[i]){
+    for (int i=1;i<=m;i++){
+        for (int j=t;j>=0;j--){
+            if (j>=w[i]){
                 dp[i][j]=max(dp[i-1][j-w[i]]+val[i],dp[i-1][j]);
-            }  
-            else{
+            }else{
                 dp[i][j]=dp[i-1][j];
             }              
         }
     }
-    printf("%d",dp[m][t]);
+    printf("%lld",dp[m][t]);
 	return 0;
 }
 

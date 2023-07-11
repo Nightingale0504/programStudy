c2ea14cf2fb70df882a7c5faa6d33e4cd4b98ac9
Boxuan Zhang <capture1969@hotmail.com>
2023-07-11 22:31:56 +0800

    Updated

--- Source Codes/[CQOI2006] 简单题/[CQOI2006] 简单题.cpp
+++ Source Codes/[CQOI2006] 简单题/[CQOI2006] 简单题.cpp
@@ -3,39 +3,39 @@
 #include<algorithm>
 using namespace std;
 #define int long long
-const int maxN=1e3+5;
+const int maxN=1e5+5;
 int n,m,opt,x,k,core;
 int tree[maxN];
 inline int lowbit(int x){
 	return (x & (-x));
 }
-inline void update(int i,int val){
+inline void update(int i){
 	while (i<=n){
-		tree[i]+=val;
+		tree[i]^=1;
 		i+=lowbit(i);
 	}
 }
 inline int sum(int i){
 	int ans=0;
 	while (i>0){
-		ans+=tree[i];
+		ans^=tree[i];
 		i-=lowbit(i);
 	}
 	return ans;
 }
 signed main(){
-//	#ifndef ONLINE_JUDGE
-//		freopen("nightingale.in","r",stdin);
-//		freopen("nightingale.out","w",stdout);
-//	#endif
+	#ifndef ONLINE_JUDGE
+		freopen("nightingale.in","r",stdin);
+		freopen("nightingale.out","w",stdout);
+	#endif
 	scanf("%lld%lld",&n,&m);
 	while (m--){
 		scanf("%lld",&opt);
 		switch (opt){
 			case 1:{
 				scanf("%lld%lld",&x,&k);
-				update(x,1);
-				update(k+1,-1);
+				update(x);
+				update(k+1);
 				break;
 			}
 			case 2:{

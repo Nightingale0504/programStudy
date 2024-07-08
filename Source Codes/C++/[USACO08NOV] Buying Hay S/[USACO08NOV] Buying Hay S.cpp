5c88077f56d94fb1ae6495b6f077799951b8f191
Nightingale0504 <capture1969@hotmail.com>
2024-07-08 23:34:09 +0800

    Upd

---
+++ Source Codes/C++/[USACO08NOV] Buying Hay S/[USACO08NOV] Buying Hay S.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<climits>
+using namespace std;
+#define int long long
+const int maxN=105,maxH=55005;
+int n,h,ans=INT_MAX;
+int p[maxN],c[maxN],dp[maxH];
+signed main(){
+	scanf("%lld%lld",&n,&h);
+	for (int i=1;i<=n;i++){
+		scanf("%lld%lld",&p[i],&c[i]);
+	}
+	for (int i=1;i<=55000;i++){
+		dp[i]=INT_MAX;
+	}
+	for (int i=1;i<=n;i++){
+		for (int j=p[i];j<=55000;j++){
+			dp[j]=min(dp[j],dp[j-p[i]]+c[i]);
+		}
+	}
+	for (int i=h;i<=55000;i++){
+		ans=min(ans,dp[i]);
+	}
+	printf("%lld",ans);
+	return 0;
+}
+

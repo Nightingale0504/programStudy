09a3658769f71a7ec23fa9be694b4056a7b8aa10
Nightingale0504 <capture1969@hotmail.com>
2024-07-12 20:47:08 +0800

    Upd

---
+++ Source Codes/C++/[NOI2000] 单词查找树/[NOI2000] 单词查找树.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<cstring>
+#include<string>
+using namespace std;
+#define int long long
+const int maxN=1e6+5;
+string s;
+int tree[maxN][26],cnt,num[maxN];
+inline void insert(string s){
+	int p=0;
+	for (int i=0;i<s.length();i++){
+		int u=s[i]-'A';
+		if (tree[p][u]==0){
+			tree[p][u]=++cnt;
+		}
+		p=tree[p][u];
+		num[p]++;
+	}
+}
+signed main(){
+	while (cin>>s){
+		insert(s);
+	}
+	cnt++;
+	printf("%lld",cnt);
+	return 0;
+}
+

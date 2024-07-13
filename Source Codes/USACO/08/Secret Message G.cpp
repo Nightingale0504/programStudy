b990f4ea002f318f7303937c675b9548e2e56b9d
Nightingale0504 <capture1969@hotmail.com>
2024-07-13 23:38:29 +0800

    Upd
    Updated something.

---
+++ Source Codes/USACO/08/Secret Message G.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+using namespace std;
+#define int long long
+const int maxN=5e5+5;
+int n,m,tree[maxN][5],num[maxN]; 
+inline void insert(string s){
+	int p=0;
+	for (int i=0;i<s.length();i++){
+		int u=s[i]-'a';
+		if (tree[p][u]==0){
+			tree[p][u]=++cnt;
+		}
+		p=tree[p][u];
+	}
+	num[p]++;
+}
+inline int query(string s){
+	int p=0;
+	for (int i=0;i<s.length();i++){
+		int u=s[i]-'a';
+		if (tree[p][u]==0){
+			return 0;
+		}else{
+			p=tree[p][u];
+		}
+	}
+	return num[p];
+}
+signed main(){
+	
+	return 0;
+}
+

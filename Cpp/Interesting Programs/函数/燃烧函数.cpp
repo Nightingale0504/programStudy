8594d4f5c739e37f6bb0f98e360bbcf99e3e368f
Nightingale0504 <capture4869@gmail.com>
2025-04-06 21:00:06 +0800

    Update

---
+++ Cpp/Interesting Programs/函数/燃烧函数.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+const int maxN=1e5+5;
+double F[maxN];
+inline double f(double x){
+	if (x<0){
+		return -x;
+	}else{
+		return 1/2*f(x-f(x-1));
+	}
+}
+signed main(){
+	double n;
+	scanf("%lf",&n);
+	for (int i=0;i<=n;i++){
+		double t=f(i);
+		printf("f(%lld)=%lf\n",i,t);
+	}
+	return 0;
+}
+

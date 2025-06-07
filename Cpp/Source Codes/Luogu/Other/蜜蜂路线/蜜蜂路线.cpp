acece3084c6e7690222f13087492c45561a713bc
Nightingale0504 <capture4869@gmail.com>
2025-06-07 16:17:20 +0800

    Update

--- Cpp/Source Codes/Luogu/Other/蜜蜂路线/蜜蜂路线.cpp
+++ Cpp/Source Codes/Luogu/Other/蜜蜂路线/蜜蜂路线.cpp
@@ -4,9 +4,10 @@ using namespace std;
 int n,m,k=1,dp[1005][2005];
 signed main(){
     scanf("%lld%lld",&m,&n);
+    int bit=n-m+1;
     dp[1][1]=1;
-    dp[2][1]=2;
-    for (int i=3;i<=n;i++){
+    dp[2][1]=1;
+    for (int i=3;i<=bit;i++){
         for (int j=1;j<=k;j++){
             dp[i][j]+=dp[i-1][j]+dp[i-2][j];
             if (dp[i][j]>=10){
@@ -14,12 +15,12 @@ signed main(){
                 dp[i][j+1]++;
             }
         }
-        if (dp[i][m+1]==1){
+        if (dp[i][k+1]>0){
             k++;
         }
     }
     for (int i=k;i>=1;i--){
-        printf("%lld",dp[n-m][i]);
+        printf("%lld",dp[bit][i]);
     }
     return 0;
 }

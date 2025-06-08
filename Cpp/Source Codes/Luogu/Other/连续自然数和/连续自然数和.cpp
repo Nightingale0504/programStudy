6aa98326c3a67ad9ce2250a6a458f789164024e5
Nightingale0504 <capture4869@gmail.com>
2025-06-08 21:00:05 +0800

    Update

--- Cpp/Source Codes/Luogu/Other/连续自然数和/连续自然数和.cpp
+++ Cpp/Source Codes/Luogu/Other/连续自然数和/连续自然数和.cpp
@@ -1,11 +1,19 @@
 #include<bits/stdc++.h>
 #define int long long
 using namespace std;
-int m;
+int m,l=1,r=1,sum;
 signed main(){
     scanf("%lld",&m);
-    for (int i=1;i<=m;i++){
-        
+    while (l<=m/2){
+        while (sum<m && r<=m){
+            sum+=r;
+            r++;
+        }
+        if (sum==m && (r-l)>=2){
+            printf("%lld %lld\n",l,r-1);
+        }
+        sum-=l;
+        l++;
     }
     return 0;
 }

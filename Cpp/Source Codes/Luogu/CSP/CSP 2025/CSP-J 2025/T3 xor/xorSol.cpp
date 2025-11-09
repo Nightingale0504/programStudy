1617cc959386984a3d1392b6faf95fae21cb47bc
Nightingale0504 <capture4869@gmail.com>
2025-11-09 12:25:56 +0800

    Update by Git Synchronization Program

--- Cpp/Source Codes/Luogu/CSP/CSP 2025/CSP-J 2025/T3 xor/xorSol.cpp
+++ Cpp/Source Codes/Luogu/CSP/CSP 2025/CSP-J 2025/T3 xor/xorSol.cpp
@@ -2,13 +2,24 @@
 #define int long long
 using namespace std;
 const int maxN=5e5+5;
-int n,k,t,sum[maxN];
+int n,k,t,sum[maxN],a[maxN];
+int l=0,cnt;
 signed main(){
     scanf("%lld%lld",&n,&k);
     for (int i=1;i<=n;i++){
         scanf("%lld",&t);
         sum[i]=sum[i-1]^t;
     }
-    
+    memset(a,-1,sizeof(a));
+    a[0]=0;
+    for (int r=1;r<=n;r++){
+        t=sum[r]^t;
+        if (a[t]>=l){
+            cnt++;
+            l=r+1;
+        }
+        a[sum[r]]=r;
+    }
+    printf("%lld",cnt);
     return 0;
 }

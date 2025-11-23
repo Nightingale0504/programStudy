6c67aa93dc88d34a86f008f6bd2432b9ad2f6f86
Nightingale0504 <capture4869@gmail.com>
2025-11-23 12:41:56 +0800

    Update by Git Synchronization Program

--- Documents/Knowledge/【知识点】KMP/【知识点】KMP.cpp
+++ Documents/Knowledge/【知识点】KMP/【知识点】KMP.cpp
@@ -1,9 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
+string s;
+int nxt[1000005];
 signed main(){
     /* KMP - Knuth-Morris-Pratt
 
     */
+    // O(n^2) 解法
+    cin>>s;
+    nxt[0]=nxt[1]=0;
+    for (int j=2;j<=s.length();j++){
+        string t=s.substr(0,j);
+        for (int len=j-1;len>=1;len--){
+            if (t.substr(0,len)==t.substr(j-len)){
+                nxt[j]=len;
+                break;
+            }
+        }
+    }
+    for (int i=0;i<=s.length();i++){
+        cout<<nxt[i]<<" ";
+    }
+
+    // O(n) 解法
+    nxt[0]=nxt[1]=0;
+    // i -> 当前最长公共前后缀长度
+    // j -> 子串长度
+    int i=0;
+    for (int j=2;j<=s.length();j++){    
+        while (i>0 && s[i]!=s[j-1]){
+            i=nxt[i];
+        }
+        if (s[i]==s[j-1]){
+            i++;
+        }
+        nxt[j]=i;
+    }
+    for (int i=0;i<=s.length();i++){
+        cout<<nxt[i]<<" ";
+    }
     return 0;
 }

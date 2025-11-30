38d7c2ed1fef109503eefe7df6e9a8958d9b2bd8
Nightingale0504 <capture4869@gmail.com>
2025-11-30 14:30:45 +0800

    Update by Git Synchronization Program

--- Cpp/Source Codes/Luogu/BalticOI/2009/[BalticOI 2009] Radio Transmission 无线传输/[BalticOI 2009] Radio Transmission 无线传输.cpp
+++ Cpp/Source Codes/Luogu/BalticOI/2009/[BalticOI 2009] Radio Transmission 无线传输/[BalticOI 2009] Radio Transmission 无线传输.cpp
@@ -3,10 +3,21 @@ using namespace std;
 #define int long long
 const int maxL=1e6+5;
 int l,length,nxt[maxL];
-string s1,s2;
+string s;
 signed main(){
     scanf("%lld",&l);
-    
+    cin>>s;
+    int i=0;
+    for (int j=2;j<=s.length();j++){
+        while (i>0 && s[i]!=s[j-1]){
+            i=nxt[i];
+        }
+        if (s[i]==s[j-1]){
+            i++;
+        }
+        nxt[j]=i;
+    }
+    length=l-nxt[l];
     printf("%lld",length);
     return 0;
 }

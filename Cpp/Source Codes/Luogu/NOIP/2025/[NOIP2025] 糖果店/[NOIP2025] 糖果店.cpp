f951ca225689a49573a0d92f757bae9d9a661183
Nightingale0504 <capture4869@gmail.com>
2025-12-07 11:59:07 +0800

    Update by Git Synchronization Program

--- Cpp/Source Codes/Luogu/NOIP/2025/[NOIP2025] 糖果店/[NOIP2025] 糖果店.cpp
+++ Cpp/Source Codes/Luogu/NOIP/2025/[NOIP2025] 糖果店/[NOIP2025] 糖果店.cpp
@@ -1,3 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
+const int maxN=1e5+5;
+int n,m,ans,k,t=LLONG_MAX;
+struct Node{
+    int x,y;
+}a[maxN];
+inline bool cmp(Node a,Node b){
+    return a.x<b.x;
+}
+signed main(){
+    scanf("%lld%lld",&n,&m);
+    for(int i=1;i<=n;i++){
+        scanf("%lld%lld",&a[i].x,&a[i].y);
+        t=min(t,a[i].x+a[i].y);
+    }
+    sort(a+1,a+n+1,cmp);
+    for (int i=0;i<=n;i++){
+        k+=a[i].x;
+        if (k>m){
+            break;
+        }
+        ans=max(ans,(m-k)/t*2+i);
+    }
+    printf("%lld",ans);
+    return 0;
+}

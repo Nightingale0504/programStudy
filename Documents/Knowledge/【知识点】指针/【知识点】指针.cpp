5ee5765745739e76aadbfe7c9c4e2415501ffd1d
Nightingale0504 <capture4869@gmail.com>
2026-03-01 11:19:15 +0800

    Update by Git Synchronization Program

---
+++ Documents/Knowledge/【知识点】指针/【知识点】指针.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+signed main(){
+    /* 指针
+        1. 定义：指针是一个变量，存储另一个变量的地址。
+        2. 取地址运算符 &
+            (1) int a=10; cout<<&a;
+        3. 解引用运算符 *, 作用: 访问指针所指向的变量
+            (1) int a=10; cout<<*(&a);
+        4. 指针
+            (1) int a=10; int *p=&a;
+    */
+    return 0;
+}

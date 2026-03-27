93bd7a67d5211bf5dc2127cd92365fbe639a054c
Nightingale0504 <capture4869@gmail.com>
2026-03-27 19:26:34 +0800

    Update by Git Synchronization Program

---
+++ Cpp/Source Codes/Luogu/CCC/CCC 2019/S2/Pretty Average Primes.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+int T;
+signed main(){
+    scanf("%lld",&T);
+    while (T--){
+        
+    }
+    return 0;
+}
--- Cpp/Source Codes/Luogu/GESP/GESP2026/03/GESP2026 03-5/有限不循环小数.cpp
+++ Cpp/Source Codes/Luogu/GESP/GESP2026/03/GESP2026 03-5/有限不循环小数.cpp
@@ -5,7 +5,16 @@ int l,r,cnt;
 signed main(){
     scanf("%lld%lld",&l,&r);
     for (int i=l;i<=r;i++){
-
+        int t=i;
+        while (t%2==0){
+            t/=2;
+        }
+        while (t%5==0){
+            t/=5;
+        }
+        if (t==1){
+            cnt++;
+        }
     }
     printf("%lld",cnt);
     return 0;

9f43b883863c124787082b09d580b84b9e033134
Nightingale0504 <capture4869@gmail.com>
2026-03-29 12:08:12 +0800

    Update by Git Synchronization Program

---
+++ Cpp/Source Codes/Luogu/语言月赛/2026/01/[语言月赛 202601] 方程检查.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+const int maxN=30;
+int q,n,tmp,jian,b[maxN];
+string s,k[maxN];
+signed main(){
+    scanf("%lld",&q);
+    while (q--){
+        scanf("%lld",&n);
+        jian=0;
+        cin>>s;
+        for (int i=0;i<n;i++){
+            cin>>k[i];
+            for (int j=0;j<k[i].size();j++){
+                
+            }
+        }
+
+    }
+    return 0;
+}

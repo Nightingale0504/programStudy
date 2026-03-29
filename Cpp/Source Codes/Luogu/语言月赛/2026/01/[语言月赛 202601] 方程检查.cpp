f002bdf7648b0af93fca18530355966f84d7196c
Nightingale0504 <capture4869@gmail.com>
2026-03-29 21:00:24 +0800

    Update by Git Synchronization Program

--- Cpp/Source Codes/Luogu/语言月赛/2026/01/[语言月赛 202601] 方程检查.cpp
+++ Cpp/Source Codes/Luogu/语言月赛/2026/01/[语言月赛 202601] 方程检查.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 #define int long long
 const int maxN=30;
-int q,n,tmp,jian,b[maxN];
+int q,n,tmp,tmp2,jian,b[maxN],a,b;
 string s,k[maxN];
 signed main(){
     scanf("%lld",&q);
@@ -12,11 +12,22 @@ signed main(){
         cin>>s;
         for (int i=0;i<n;i++){
             cin>>k[i];
+            tmp=k[i][0]-'a'+1;
+            k[i].erase(0,2);
             for (int j=0;j<k[i].size();j++){
+                tmp2=tmp2*10+k[i][j]-'0';
+            }
+            b[tmp]=tmp2;
+            tmp2=0;
+        }
+        for (int i=0;i<s.size();i++){
+            if (s[i]=='-'){
+                jian=1;
+            }
+            if (jian){
                 
             }
         }
-
     }
     return 0;
 }
